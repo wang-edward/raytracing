86e35ce047dc9c2e697c68053d26df3d962b28bd
wang-edward <edawordwang@gmail.com>
2023-12-23 21:55:12 -0500

    upload

---
+++ main.cc
@@ -0,0 +1,21 @@
+#include "rtweekend.h"
+
+#include "camera.h"
+#include "hittable_list.h"
+#include "sphere.h"
+
+int main() {
+    hittable_list world;
+
+    world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
+    world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
+
+    camera cam;
+
+    cam.aspect_ratio = 16.0 / 9.0;
+    cam.image_width  = 1600;
+    cam.samples_per_pixel = 100;
+    cam.samples_per_pixel = 50;
+
+    cam.render(world);
+}
